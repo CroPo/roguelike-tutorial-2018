c47415cfdff6924b1ec69e7908d6a5570f2a4706
CroPo <spindler.reinmar+github@gmail.com>
2018-07-30 18:53:12 +0200

    Create part_10 from part_9

---
+++ part_10/README.md
@@ -0,0 +1,10 @@
+# Part 10: Saving and Loading
+
+- [Reddit Post on /r/roguelikedev](https://www.reddit.com/r/roguelikedev/comments/91do0i/roguelikedev_does_the_complete_roguelike_tutorial/)
+- [Direct Tutorial Link](http://rogueliketutorials.com/libtcod/10)
+
+So, we're starting with loading and saving the game here. This time, I can just loosely resemble what the Python
+tutorial does due to the obvious difference of both languages. Since I already have done a part of what the tutorial
+wanted me to do - namely move all the game logic to another class - I don't expect much problems at all.
+
+Contents of this Writeup:
---
+++ part_10/src/message.rs
@@ -0,0 +1,39 @@
+use tcod::Color;
+use tcod::colors;
+use std::cell::RefCell;
+use std::cell::Ref;
+
+pub struct Message {
+    pub text: String,
+    pub color: Color,
+}
+
+impl Message {
+    pub fn new(text: String, color: Color) -> Message {
+        Message {
+            text,
+            color,
+        }
+    }
+}
+
+pub struct MessageLog {
+    messages: RefCell<Vec<Message>>
+}
+
+impl MessageLog {
+    pub fn new() -> MessageLog {
+        MessageLog {
+            messages: RefCell::new(vec![])
+        }
+    }
+
+    pub fn add(&self, message: Message) {
+        self.messages.borrow_mut().push(message);
+    }
+
+    pub fn messages(&self) -> Ref<Vec<Message>> {
+        self.messages.borrow()
+    }
+}
+
